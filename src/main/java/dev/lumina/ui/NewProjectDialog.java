e6a30a4eb2ae7980f3b77612132bdb7a8ea47efd
firoze-hossain <firoze.hossain@outlook.com>
2026-07-30 19:00:55 +0600

    sidebar menu with grade maven option added

--- src/main/java/dev/lumina/ui/NewProjectDialog.java
+++ src/main/java/dev/lumina/ui/NewProjectDialog.java
@@ -1,22 +1,36 @@
 package dev.lumina.ui;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Consumer;
+
 import dev.lumina.project.ProjectSpec;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
 
 /**
  * IntelliJ-style "New Project" dialog:
